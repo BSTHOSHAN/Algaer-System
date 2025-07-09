d5dd3a5f56fe1ca3fcd54766f9f0313810476c1d
BSTHOSHAN <bsthoshan@gmail.com>
2025-07-09 17:50:23 -0500

    Hello

--- src/main/java/subsystems/Algaer/intakingSystem.java
+++ src/main/java/subsystems/Algaer/intakingSystem.java
@@ -1,12 +1,14 @@
 package subsystems.Algaer;
 import static edu.wpi.first.units.Units.Newton;
 
+
 import com.ctre.phoenix6.hardware.TalonFX;
+import org.team7525.subsystem.Subsystem;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.motorcontrol.Talon;
 
-public class intakingSystem {
+public class intakingSystem extends Subsystem<AlgaerStates>{
     TalonFX Pivotmotor = new TalonFX(1);
     TalonFX Wheelmotor = new TalonFX(2);
     PIDController PivotPID = new PIDController(1, 0., 0);
@@ -16,6 +18,7 @@ public class intakingSystem {
         Pivotmotor.setVoltage(PivotPID.calculate(Pivotmotor.getPosition().getValueAsDouble(), Currentstate.getTargetSpeed()));
         Wheelmotor.setVoltage(WheelPID.calculate(Wheelmotor.getPosition().getValueAsDouble(), Currentstate.getTargetPosition()));
     }
+
     public void setState(AlgaerStates state) {
         this.Currentstate = state;
         Pivotmotor.setVoltage(PivotPID.calculate(Pivotmotor.getPosition().getValueAsDouble(), Currentstate.getTargetSpeed()));
